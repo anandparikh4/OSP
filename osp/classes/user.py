4e611ffcf61731f7f6cc23e09f2288a50bdb6f61
adityasoni9998 <adityasoni9998@gmail.com>
2022-03-27 20:50:15 +0530

    updated user.py

--- osp/classes/user.py
+++ osp/classes/user.py
@@ -13,7 +13,7 @@ class User(Document):
     name = StringField(required = True,minlength = 1)
     email = EmailField(required = True)
     address = ReferenceField(Address,required=True,reverse_delete_rule=NULLIFY)
-    telephone = IntField(required = True,min_value=1000000000,max_value=9999999999)
+    telephone = IntField(required=True, min_value=1000000000, max_value=9999999999)
 
     def change_data(self,**kwargs):
         try:
@@ -29,68 +29,69 @@ class User(Document):
             return (True, "Profile information updated successfully")
 
         except Exception as ex:
-            return(False , str(ex))
+            return False, str(ex)
 
-    def change_password(self,oldpass,newpass):
+    def change_password(self,oldpass, newpass):
         try:
             if oldpass != self.password :
                 raise Exception("Wrong password!")
             self.password = newpass
             self.save()
-            return (True,"Password changed")
+            return True, "Password changed"
 
         except Exception as e:
-            return(False,str(e))
+            return False,str(e)
 
 
 
 class Manager(User):
-    gender = StringField(default = "MALE" , choices = GENDER)
-    dob = DateField(required = True)
+    gender = StringField(required=True, choices=GENDER)
+    dob = DateField(required=True)
 
-    def type(self):                         # mimicking static variables
+    def type(self):
         return "Manager"
 
-    def signup_key(self):
-        key = str("for_managers_only")
-        return key
+    @staticmethod
+    def signup_key():                       # mimicking static variables
+        return str("for_managers_only")
 
-    def change_category(self,item_id,category_id):
+
+    def change_category(self, item_id, category_id):
 
         try:
-            item = Item.objects(uid = item_id).first()
-            if not item :
+            item_ = Item.objects(uid=item_id).first()
+            if not item_:
                 raise Exception("No such item found!")
 
-            category = Category.objects(uid = category_id).first()
-            if not category :
+            category_ = Category.objects(uid=category_id).first()
+            if not category_:
                 raise Exception("No such item found!")
 
-            item.category = category
-
+            item_.category = category_
+            item_.save()
         except Exception as e:
-            return(False,str(e))
+            return False,str(e)
+
 
     # for adding and removing categories, the static add and remove categories of the class Category can be used directly. No special methods are required in class Manager
-
     # similarly for deleting items, the static method delete item of class Item can be used directly
 
 
     def manage_seller(self,seller_id):
         try:
-            seller = Seller.objects(uid = seller_id)
+            seller = Seller.objects(uid=seller_id).first()
             if seller:
                 seller.delete()
-                return(True, "Seller deleted")
+                return True, "Seller deleted"
             else :
                 raise Exception("No such seller found")
 
         except Exception as e:
-            return (False, str(e))
+            return  False, str(e)
 
     def manage_buyer(self, buyer_id):
         try:
-            buyer = Buyer.objects(uid=buyer_id)
+            buyer = Buyer.objects(uid=buyer_id).first()
             if buyer:
                 buyer.delete()
                 return (True, "Buyer deleted")
@@ -98,27 +99,26 @@ class Manager(User):
                 raise Exception("No such buyer found")
 
         except Exception as e:
-            return (False, str(e))
+            return  False, str(e)
 
 
     #def audit(self):    # implement after class Buy Requests
 
     #def negotiations(self,seller_id,buyer_id):   # implement after class Buy Requests
 
-
-class Seller(User):
-    # for adding and deleting products, static methods of Class Item can be used
-    def view_pending_orders(self):
-        return Order.objects(seller = self)
-
-    def view_sales(self):
-        return Transaction.objects(seller = self)
-
-    def negotiate(self,offer_price):
-        pass
-
-class Buyer:
-    pass
+#
+# class Seller(User):
+#     # for adding and deleting products, static methods of Class Item can be used
+#     def view_pending_orders(self):
+#         return Order.objects(seller=self)
+#
+#     def view_sales(self):
+#         return Transaction.objects(seller = self)
+#
+#     def negotiate(self,offer_price):
+#         pass
+#
+# class Buyer(User):
 
 
 
