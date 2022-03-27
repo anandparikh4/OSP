bac7f5d08686e1ad3e193b148c6ef0cd38bc6e9b
adityasoni9998 <adityasoni9998@gmail.com>
2022-03-27 21:29:11 +0530

    Update order.py

--- osp/classes/order.py
+++ osp/classes/order.py
@@ -1,30 +1,45 @@
-from mongoengine import FloatField,BooleanField,ReferenceField,StringField,CASCADE
+from mongoengine import FloatField,BooleanField,ReferenceField,StringField,ImageField,CASCADE
 from osp.classes.item import Item
 from osp.classes.user import Seller,Buyer
 
 REQUEST_STATUS = ("Pending","Rejected","Accepted")
 
 class Order:
-    offer_price = FloatField(required = True,min_value=0)
+    offer_price = FloatField(required=True,min_value=0)
     item = ReferenceField(Item,reverse_delete_rule=CASCADE,required = True)
     buyer = ReferenceField(Buyer,reverse_delete_rule=CASCADE,required = True)
-    seller = ReferenceField(Seller , reverse_delete_rule=CASCADE,required = True)
+    seller = ReferenceField(Seller,reverse_delete_rule=CASCADE,required = True)
     request_status = StringField(default="Pending",choices = REQUEST_STATUS)
     delivery_status = BooleanField(default = False)
     payment_status = BooleanField(default = False)
 
-    def negotiate(self,offer):
+    @staticmethod
+    def create_order(**kwargs):
+        try:
+            new_order = Order
+
+    def negotiate(self, offer):
         try:
             if offer < 0:
                 raise Exception("Please enter a valid offer price")
             self.offer_price = offer
-            return (True , "Offer placed")
+            return True, "Offer placed"
 
         except Exception as e:
-            return (False , str(e))
+            return False, str(e)
 
 class Transaction:
-    offer_price = FloatField(required = True,min_value=0)
-    item = ReferenceField(Item,reverse_delete_rule=CASCADE,required = True)
-    buyer = ReferenceField(Buyer,reverse_delete_rule=CASCADE,required = True)
-    seller = ReferenceField(Seller , reverse_delete_rule=CASCADE,required = True)
+    offer_price = FloatField(required = True, min_value=0)
+    item_name = StringField(required=True, min_length=1)
+    item_id = StringField(required=True,min_length=1)
+    buyer_name = StringField(required=True,min_length=1)
+    buyer_id = StringField(required=True,min_length=1)
+    seller_name = StringField(required=True,min_length=1)
+    seller_id = StringField(required=True,min_length=1)
+    photo = ImageField(size=(300,300,True),required=True)
+    category_name = StringField(required=True,min_length=1)
+    category_id = StringField(required=True,min_length=1)
+
+    @staticmethod
+    def create_transaction(order):
+        new_transaction =
