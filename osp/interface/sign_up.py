e982495fcdb2ccc80eb8292942fe392903623e51
adityasoni9998 <adityasoni9998@gmail.com>
2022-03-28 17:45:17 +0530

    i am trying

--- osp/interface/sign_up.py
+++ osp/interface/sign_up.py
@@ -1,15 +1,18 @@
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
+from decouple import config
 
 
-def send_email(subject,text,send_address):
+def send_email(subject,text,receive_address):
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('noreplyospgrp8@gmail.com', 'osp_password')
+    sender_mail = config('USERNAME')
+    sender_password = config('PASSWORD')
+    smtp.login(sender_mail, sender_password)
     message = MIMEMultipart()
     message['Subject'] = subject
     message.attach(MIMEText(text))
-    smtp.sendmail(from_addr='noreplyospgrp8@gmail.com',to_addrs=send_address, msg=message.as_string())
+    smtp.sendmail(from_addr='noreplyospgrp8@gmail.com',to_addrs=receive_address, msg=message.as_string())
     smtp.quit()
