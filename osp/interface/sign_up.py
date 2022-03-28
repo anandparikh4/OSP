6ee7d85150d60e6a7be3900ad0c0bb767787a262
adityasoni9998 <adityasoni9998@gmail.com>
2022-03-28 19:12:55 +0530

    commit

--- osp/interface/sign_up.py
+++ osp/interface/sign_up.py
@@ -16,15 +16,15 @@ def send_email(subject,text,receive_address):
         message.attach(MIMEText(text))
         smtp.sendmail(from_addr=config('MAIL'),to_addrs=receive_address, msg=message.as_string())
         smtp.quit()
-        return True
-    except Exception as ex:
-        return False,str(ex)
+        return True,"Mail sent successfully"
+    except:
+        raise
 
 def manager_sign_up(**kwargs):
     from osp.classes.user import Manager
     try:
         new_manager = Manager.create_manager(**kwargs)
-        return new_manager
+        return True,new_manager
     except Exception as ex:
         return False, str(ex)
 
@@ -32,7 +32,7 @@ def seller_sign_up(**kwargs):
     from osp.classes.user import Seller
     try:
         new_seller = Seller.create_seller(**kwargs)
-        return new_seller
+        return True,new_seller
     except Exception as ex:
         return False, str(ex)
 
@@ -40,6 +40,6 @@ def buyer_sign_up(**kwargs):
     from osp.classes.user import Buyer
     try:
         new_buyer = Buyer.create_buyer(**kwargs)
-        return new_buyer
+        return True,new_buyer
     except Exception as ex:
         return False, str(ex)
