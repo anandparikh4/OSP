8e36aa1006d2eb61cddab3a9537e8700165754f6
adityasoni9998 <adityasoni9998@gmail.com>
2022-03-28 13:52:51 +0530

    Update __init__.py

--- osp/classes/__init__.py
+++ osp/classes/__init__.py
@@ -1,5 +1,5 @@
 from osp.classes.address import Address
-from osp.classes.category import  Category
+from osp.classes.category import Category
 from osp.classes.item import Item
 from osp.classes.order import Order,Transaction
 from osp.classes.user import User, Manager, Buyer, Seller
