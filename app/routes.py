cfefbfbe165639c56eaa4cfb882c29d3a4223dea
anandparikh4 <98024272+anandparikh4@users.noreply.github.com>
2022-03-29 17:58:20 +0530

    Initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,16 @@
 from app import app
-from flask import render_template,redirect,url_for,request
+from flask import render_template,redirect,url_for,request,flash
 import mongoengine as ming
 #from osp.classes.user import User
 from osp.classes.address import Address
+from osp.interface.sign_up import buyer_sign_up,seller_sign_up
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template("home.html")
 
+'''
 @app.route("/market", methods=["POST"])
 def market_page():
     residence_number = "1"
@@ -26,3 +28,24 @@ def market_page():
 def about_page(username):
 
     return f"<p>about of {username}</p>"
+'''
+
+
+@app.route("/sign_in" , methods = ["GET" ,"POST"])
+def sign_in():
+    req = request.form
+    print(req.keys())
+    return render_template("sign_in.html")
+
+
+@app.route("/sign_up" , methods = ["GET" ,"POST"])
+def sign_up():
+    req = request.form
+    print(req.keys())
+    return render_template("sign_up.html")
+
+@app.route("/manager_sign_up" , methods = ["GET" ,"POST"])
+def manager_sign_up():
+    req = request.form
+    print(req.keys())
+    return render_template("manager_sign_up.html")
