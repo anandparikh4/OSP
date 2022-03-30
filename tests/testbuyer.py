9522bc3e396c9699265c106478d443d61a775dbd
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2022-03-30 22:08:05 +0530

    testbuyer.py

--- tests/testbuyer.py
+++ tests/testbuyer.py
@@ -8,7 +8,7 @@ address = Address(residence_number="D/704, Lodha Meridian", street="Kukatpally",
 try:
     c1=Buyer(password="1234",name="test_buyer", email="iitkgp@email.com", address = address, telephone = "8886077670")
     c1.save()
-    c1.uniqueid=str(c1.id)
+    c1.uid=str(c1.id)
     c1.save()
     # We print passed if no exception occurs
     print("Passed without acception as correct parameters given")
@@ -19,10 +19,10 @@ print("Constructor with incorrect parameters")
 try:
     c1=Buyer(password="abcd",name="", email="testemail@email.com",address = address, telephone="9")
     c1.save()
-    c1.uniqueid=str(c1.id)
+    c1.uid=str(c1.id)
     c1.save()
     print("Failed")
-    Buyer.objects(uniqueid=c1.uniqueid).first().delete()
+    Buyer.objects(uid=c1.uid).first().delete()
 except:
     # Print passed if exception occurs as the main aim of this part is to print exception as wrong paramters are given
     print("passed")
@@ -30,7 +30,7 @@ except:
 
 print("Deletion of buyer")
 try:
-    Buyer.objects(uniqueid=c1.uniqueid).first().delete()
+    Buyer.objects(uid=c1.uid).first().delete()
     print("Passed")
 except Exception as e:
     print("failed",e)
