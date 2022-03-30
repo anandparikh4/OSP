95126c05b94b0f049b2663ec2d0c5f9eafe1c822
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2022-03-30 21:55:39 +0530

    Create test buyer

---
+++ tests/testbuyer.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append(".")
+
+from osp.classes.user import Buyer
+from osp.classes.address import Address
+print("Checking the constructor")
+address = Address(residence_number="D/704, Lodha Meridian", street="Kukatpally", locality="KPHB", city="Hyderabad", state="Andhra Pradesh", pincode="500072")
+try:
+    c1=Buyer(password="1234",name="test_buyer", email="iitkgp@email.com", address = address, telephone = "8886077670")
+    c1.save()
+    c1.uniqueid=str(c1.id)
+    c1.save()
+    # We print passed if no exception occurs
+    print("Passed without acception as correct parameters given")
+except Exception as e:
+    print("Failed",e)
+
+print("Constructor with incorrect parameters")
+try:
+    c1=Buyer(password="abcd",name="", email="testemail@email.com",address = address, telephone="9")
+    c1.save()
+    c1.uniqueid=str(c1.id)
+    c1.save()
+    print("Failed")
+    Buyer.objects(uniqueid=c1.uniqueid).first().delete()
+except:
+    # Print passed if exception occurs as the main aim of this part is to print exception as wrong paramters are given
+    print("passed")
+
+
+print("Deletion of buyer")
+try:
+    Buyer.objects(uniqueid=c1.uniqueid).first().delete()
+    print("Passed")
+except Exception as e:
+    print("failed",e)
