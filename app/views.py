56d967605fd2a84024210c909c550ff5b6a13012
anandparikh4 <98024272+anandparikh4@users.noreply.github.com>
2022-03-30 12:21:17 +0530

    Initial commit

--- app/views.py
+++ app/views.py
@@ -151,19 +151,19 @@ def sign_in():
                 user1.is_authenticated = True
                 user1.save()
                 login_user(user1)
-                return redirect("/welcome")
+                return redirect("/manager")
 
             elif user2:
                 user2.is_authenticated = True
                 user2.save()
                 login_user(user2)
-                return redirect("/welcome")
+                return redirect("/buyer")
 
             elif user3:
                 user3.is_authenticated = True
                 user3.save()
                 login_user(user3)
-                return redirect("/welcome")
+                return redirect("/seller")
 
             if ( (not user1) and (not user2) and (not user3) ):
                 raise Exception("Invalid login credentials")
@@ -176,10 +176,109 @@ def sign_in():
 
 @app.route("/sign_out")
 @login_required
-def logout():
-    user = current_user
-    user.is_authenticated = False
-    user.save()
+def sign_out():
+    curr_user = current_user
+    curr_user.is_authenticated = False
+    curr_user.save()
     logout_user()
     return redirect("/sign_in")
+
+
+# manager routes
+@app.route("/manager" , methods = ["GET" ,"POST"])
+@is_manager
+@login_required
+def manager():
+    return render_template("manager/manager.html")
+
+@app.route("/manager/manage_buyers" , methods = ["GET" ,"POST"])
+@is_manager
+@login_required
+def manage_buyers():
+    return render_template("manager/manage_buyers.html")
+
+@app.route("/manager/manage_sellers" , methods = ["GET" ,"POST"])
+@is_manager
+@login_required
+def manage_sellers():
+    return render_template("manager/manage_sellers.html")
+
+@app.route("/manager/audit" , methods = ["GET" ,"POST"])
+@is_manager
+@login_required
+def audit():
+    return render_template("manager/audit.html")
+
+@app.route("/manager/help_negotiations" , methods = ["GET" ,"POST"])
+@is_manager
+@login_required
+def help_negotiations():
+    return render_template("manager/help_negotiations.html")
+
+
+# seller routes
+@app.route("/seller" , methods = ["GET" ,"POST"])
+@is_seller
+@login_required
+def seller():
+    return render_template("seller/seller.html")
+
+@app.route("/seller/buy_requests" , methods = ["GET" ,"POST"])
+@is_seller
+@login_required
+def buy_requests():
+    return render_template("seller/buy_requests.html")
+
+@app.route("/seller/items" , methods = ["GET" ,"POST"])
+@is_seller
+@login_required
+def items():
+    return render_template("seller/items.html")
+
+@app.route("/seller/upload_item" , methods = ["GET" ,"POST"])
+@is_seller
+@login_required
+def upload_item():
+    return render_template("seller/upload_item.html")
+
+@app.route("/seller/sales" , methods = ["GET" ,"POST"])
+@is_seller
+@login_required
+def sales():
+    return render_template("seller/sales.html")
+
+
+# buyer routes
+
+@app.route("/buyer" , methods = ["GET" ,"POST"])
+@is_buyer
+@login_required
+def buyer():
+    return render_template("buyer/buyer.html")
+
+@app.route("/buyer/purchase_requests" , methods = ["GET" ,"POST"])
+@is_buyer
+@login_required
+def purchase_requests():
+    return render_template("buyer/purchase_requests.html")
+
+@app.route("/buyer/purchases" , methods = ["GET" ,"POST"])
+@is_buyer
+@login_required
+def purchases():
+    return render_template("buyer/purchases.html")
+
+
+
+###
+# dashboard
+
+@app.route("/dashboard" , methods = ["GET" ,"POST"])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
+
+
 
